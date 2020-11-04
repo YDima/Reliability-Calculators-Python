77450e36a4f6261d8ddc49e4c02b911a78c31739
YDima3 <yurchenkodmytroofficial@gmail.com>
2020-11-04 21:15:17 +0200

    Working calculator

--- problem1.py
+++ problem1.py
@@ -1,1 +1,19 @@
-333
+import math
+
+print("Enter Cumulative Fault Frequency:")
+cff = float(input())
+print("Enter Magnitude of life:")
+t = float(input())
+
+def find_r (cff):
+    r = math.exp(-cff)
+    r = round(r, 3)
+    r_percents = str(r*100) + "%"
+    print(f"R = {r_percents}")
+
+def find_mtbf (cff, t):
+    mmbf = t/cff
+    print(f"MMBF = {mmbf} Miles/fault during {t} miles")
+
+find_r(cff)
+find_mtbf(cff, t)
