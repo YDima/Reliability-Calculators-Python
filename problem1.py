014204b5e61b3b8bc59a58a9c0da06cef8969581
YDima3 <yurchenkodmytroofficial@gmail.com>
2020-11-04 21:18:42 +0200

    Working calculator

--- problem1.py
+++ problem1.py
@@ -7,8 +7,8 @@ t = float(input())
 
 def find_r (cff):
     r = math.exp(-cff)
-    r = round(r, 3)
-    r_percents = str(r*100) + "%"
+    r = str(round(r*100, 3))
+    r_percents = r + "%"
     print(f"R = {r_percents}")
 
 def find_mtbf (cff, t):
